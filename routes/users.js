129d5d22d8258a16fd067be1de8d17f71daa5ed6
Jarvis <jarvishhc@gmail.com>
2019-07-02 06:29:35 +0800

    Complete registration functionality

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,41 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+// Register validation
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  // Check whether user already exists
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+
+      // bcrypt password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+
+          newUser
+            .save()
+            .then(user => {
+              res.redirect('/')
+            }).catch(err => console.log(err))
+        })
+      })
+    }
+  })
+})
+
 module.exports = router
